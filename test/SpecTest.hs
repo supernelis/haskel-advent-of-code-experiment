b6573cedb291e543198bdbc4c7e4b4c0f21dd915
Nelis Boucké <nelis@archiwise.com>
2022-01-12 20:35:47 +0100

    fake readInputFile

--- test/SpecTest.hs
+++ test/SpecTest.hs
@@ -8,7 +8,7 @@ import Test.Hspec
 import Basic
 
 main :: IO ()
-main = hspec simpleMathSpec
+main = hspec day1Spec
 
 -- TODO: Add more cases!
 simpleMathSpec :: Spec
@@ -16,3 +16,11 @@ simpleMathSpec = describe "Tests of our simple math function" $ do
   context "when the numbers are small" $
     it "Should match the our expected value" $
       simpleMathFunction 3 4 5 `shouldBe` 7
+
+readInputFile = [191]
+
+day1Spec :: Spec
+day1Spec = describe "foo" $ do
+  context "blah" $
+    it "should do something" $
+      head readInputFile `shouldBe` 191
