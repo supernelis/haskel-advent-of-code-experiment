4a03f17e5324fa5c0faec31bf4dcad322720de21
Nelis Boucké <nelis@archiwise.com>
2022-01-18 20:03:44 +0100

    Cleanup spec

--- test/SpecTest.hs
+++ test/SpecTest.hs
@@ -11,17 +11,9 @@ import System.IO
 main :: IO ()
 main = hspec day1Spec
 
--- TODO: Add more cases!
-simpleMathSpec :: Spec
-simpleMathSpec = describe "Tests of our simple math function" $ do
-  context "when the numbers are small" $
-    it "Should match the our expected value" $
-      simpleMathFunction 3 4 5 `shouldBe` 7
-
 readInputFile = do
   rawcontent <- readFile "test/input_day1"
   return (lines rawcontent)
-  -- content <- (readFile "input_day1")
   
 day1Spec :: Spec
 day1Spec = describe "day1" $ do
