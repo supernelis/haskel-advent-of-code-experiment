e8eb02f80123e625f4b00bc92cbfdac5382087cd
Nelis Boucké <nelis@archiwise.com>
2022-01-27 21:08:10 +0100

    Introduce a submarine with position and depth

--- test/Day2_SpecTest.hs
+++ test/Day2_SpecTest.hs
@@ -11,10 +11,18 @@ import System.IO
 main :: IO ()
 main = hspec day2Spec
 
-data HPosition = Position Int
+data Position = Position Int
+  deriving (Show, Eq)
+
+data Depth = Depth Int
+  deriving (Show, Eq)
+
+data Submarine = Submarine {position :: Position, depth :: Depth}
   deriving (Show, Eq)
 
 forward (Position origin) steps = Position (origin + steps)
+
+forward2 (Submarine (Position x) (Depth d)) steps = Submarine (Position 5) (Depth 0)
 
 day2Spec :: Spec
 day2Spec = describe "day2" $ do
@@ -25,4 +33,6 @@ day2Spec = describe "day2" $ do
       forward (Position 5) 5 `shouldBe` Position 10
     it "should move two times forward" $
       forward (forward (Position 0) 5) 5 `shouldBe` Position 10
+    it "should move submarine" $
+      forward2 (Submarine (Position 0) (Depth 0)) 5 `shouldBe` Submarine (Position 5) (Depth 0)
   
