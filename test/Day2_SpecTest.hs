a28520e5a2967e1600eb73b51b6eef9de2ed8c5f
Nelis Boucké <nelis@archiwise.com>
2022-01-27 20:55:10 +0100

    add second test

--- test/Day2_SpecTest.hs
+++ test/Day2_SpecTest.hs
@@ -18,7 +18,9 @@ forward (Position origin) steps = Position (origin + steps)
 
 day2Spec :: Spec
 day2Spec = describe "day2" $ do
-  context "" $
+  context "" $ do
     it "should move the submarine forward" $
       forward (Position 0) 5 `shouldBe` Position 5
+    it "should move from 5 to 10" $
+      forward (Position 5) 5 `shouldBe` Position 10
   
