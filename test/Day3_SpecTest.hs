dc52fe7b09adfea290048c012fc4b00be6aac809
Nelis Boucké <nelis@archiwise.com>
2022-03-31 20:40:06 +0200

    Check column sum and length

--- test/Day3_SpecTest.hs
+++ test/Day3_SpecTest.hs
@@ -12,12 +12,20 @@ main :: IO ()
 main = do
   hspec day3Spec 
 
-gamma column = sum column
+gamma column = do
+    let columnSum = sum column
+    let columnLength = length column
+    if columnSum > (columnLength - columnSum) 
+        then 1
+        else 0
 
 
 day3Spec :: Spec
 day3Spec = describe "day3" $ do
   describe "something" $ do
     it "gamma" $ do
-      let first_column = [0, 0]
+      let first_column = [0, 0, 1]
       gamma first_column `shouldBe` 0
+    it "sums list" $ do
+      let first_column = [1, 0, 1]
+      gamma first_column `shouldBe` 1
